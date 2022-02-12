a992e1cf9f31b9e4637f34ffdd19533061909ee6
nilldiggonto <nilldiggonto@gmail.com>
2022-02-13 05:27:27 +0600

    alembic setup done

---
+++ fastapi_docker/db/models.py
@@ -0,0 +1,31 @@
+from operator import index
+from matplotlib.pyplot import title
+from sqlalchemy import Column,String,Integer,DateTime,ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id      =   Column(Integer,primary_key=True,index=True)
+    title   =   Column(String)
+    rating  =   Column(Integer)
+    created =   Column(DateTime(timezone=True),server_default=func.now())
+    updated =   Column(DateTime(timezone=True),onupdate=func.now())
+    
+    authord_id  =   Column(Integer,ForeignKey('author.id'))
+    author      =   relationship("Author")
+
+class Author():
+    __tablename__ = 'author'
+
+    id      =   Column(Integer,primary_key=True,index=True)
+    name    =   Column(String)
+    age     =   Column(Integer)
+    created =   Column(DateTime(timezone=True),server_default=func.now())
+    updated =   Column(DateTime(timezone=True),onupdate=func.now())
+    
